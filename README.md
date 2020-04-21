db88d126ab59d9e9fea358d8ffb1335449f84e4e
fedjoyd5 <53665065+fedjoyd5@users.noreply.github.com>
2020-04-21 15:54:07 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # deckboard-webrequest
+Shutdown and restart functions for Deckboard
+
+## Install
+- Clone this repository in Users/%USER%/deckboard/extensions directory.
+`` git clone https://github.com/fedjoyd5/deckboard-webrequest.git ``
+
+- Do ``npm install`` in newly created deckboard-webrequest folder
+
+- Restart Deckboard
---
+++ index.js
@@ -0,0 +1,44 @@
+const { dialog } = require('electron');
+const { Extension, log, INPUT_METHOD, PLATFORMS } = require('deckboard-kit');
+const https = require('https');
+
+class WebRequestExtension extends Extension {
+	constructor() {
+		super();
+		this.name = 'API Web Request';
+		this.platforms = [PLATFORMS.WINDOWS, PLATFORMS.MAC];
+		this.inputs = [
+			{
+				label: 'GET WebRequest',
+				value: 'GET-WebRequest',
+				icon: 'keyboard',
+				color: '#34495e',
+				input: [
+					{
+						type: INPUT_METHOD.INPUT_TEXT
+						label: 'URL',
+						ref: 'URL',
+					}
+				]
+			}
+		];
+	}
+
+	execute(action, { URL }) {
+		//log.info(`${action} ${powerAction}`);
+		switch (action) {
+			case 'GET-WebRequest': {
+				https.get(( URL.startswith("https://") ? URL : ("https://" + URL)), (resp) => {
+					resp.on('end', () => {
+						log.info("request ended");
+					});
+				}).on("error", (err) => { log.info("Error: " + err.message); });
+
+			}
+			default:
+				break;
+		}
+	}
+}
+
+module.exports = new WebRequestExtension();
